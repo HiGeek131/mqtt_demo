b43f4dfe513efdc9c6b39a3f49e3e566e4529c6c
Gary <2801079570@qq.com>
2020-09-10 16:25:23 +0800

    建立服务程序

---
+++ mqtt_client/src/server.java
@@ -0,0 +1,34 @@
+public class server {
+    public static void main(String[] args) {
+        MyThread myThread = new MyThread("task1");
+        myThread.start();
+    }
+}
+
+class MyThread extends Thread {
+    private String threadName;
+    public MyThread(String threadName) {
+        this.threadName = threadName;
+    }
+
+    @SuppressWarnings("InfiniteLoopStatement")
+    @Override
+    public void run() {
+        System.out.println(threadName + " run");
+        MyMQTT myMQTT = new MyMQTT("tcp://higeekstudio.cn:11883", "test0");
+        myMQTT.setPassWord("t", "t");
+        if (myMQTT.connect()) {
+            System.out.println("connect success");
+        } else {
+            System.out.println("connect error");
+        }
+        while (true) {
+            myMQTT.publishMessage("test/", "test message");
+            try {
+                MyThread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
